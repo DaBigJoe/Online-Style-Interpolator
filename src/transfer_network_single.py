a443aa03986b8fc7c3eade5c6e45ef447793887f
Joe Early <je5g15@soton.ac.uk>
2019-05-13 20:40:30 +0100

    Implemented dataloader for transfer network single so network is now trained against dataset of content images.

--- src/transfer_network_single.py
+++ src/transfer_network_single.py
@@ -17,6 +17,10 @@ from tqdm import tqdm
 from image_handler import load_image_as_tensor, save_tensor_as_image, plot_image_tensor, transform_256, \
     save_tensors_as_grid
 from loss_network import LossNetwork
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.utils import data
+from os import listdir
 
 
 class ResidualBlock(torch.nn.Module):
@@ -98,18 +102,47 @@ class TransferNetworkSingle(torch.nn.Module):
         return y
 
 
+class Dataset(data.Dataset):
+
+    def __init__(self, image_dir):
+        self.data = []
+
+        items = listdir(image_dir)
+
+        self.image_dir = image_dir
+        self.data = items
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        X = load_image_as_tensor(self.image_dir + self.data[index], transform=transform_256).squeeze(0)
+        if not (len(X) == 3):
+            _X = torch.zeros((3, X.shape[1], X.shape[2]))
+            _X[0] = X[0]
+            _X[1] = X[0]
+            _X[2] = X[0]
+            X = _X
+        return X
+
+
 class TransferNetworkTrainerSingle:
 
-    def __init__(self, style_path, content_path, save_directory):
+    def __init__(self, style_path, content_dir, save_directory, test_image_path):
         print('Creating single transfer network')
         self.style_path = style_path
-        self.content_path = content_path
 
-        device = "cuda:0" if torch.cuda.is_available() else "cpu"
+        self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-        # Load images as tensors
-        self.style_tensor = load_image_as_tensor(self.style_path, transform=transform_256).to(device)
-        self.content_tensor = load_image_as_tensor(self.content_path, transform=transform_256).to(device)
+        # Load content images
+        print('Loading content images')
+        train_dataset = Dataset(content_dir)
+        self.train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True)
+        print('Found', len(self.train_loader), 'images')
+        self.test_image_tensor = load_image_as_tensor(test_image_path, transform=transform_256).to(self.device)
+
+        # Load style
+        self.style_tensor = load_image_as_tensor(self.style_path, transform=transform_256).to(self.device)
 
         # Load loss network
         self.loss_network = LossNetwork()
@@ -124,12 +157,11 @@ class TransferNetworkTrainerSingle:
         if not os.path.exists(self.save_directory):
             os.makedirs(self.save_directory)
 
-    def train(self, epochs=1000, num_checkpoints=10):
+    def train(self, num_parameter_updates=40000, num_checkpoints=9):
         print('Training single transfer network')
-        model = TransferNetworkSingle().cuda()
+        model = TransferNetworkSingle().to(self.device)
         optimiser = optim.Adam(model.parameters(), lr=1e-3)
-
-        checkpoint_freq = epochs // num_checkpoints
+        checkpoint_freq = num_parameter_updates // num_checkpoints
 
         # Weight of style vs content
         style_weight = 1e12
@@ -137,38 +169,52 @@ class TransferNetworkTrainerSingle:
 
         # Train
         checkpoint = 0
-        epoch_iter = tqdm(range(1, epochs+1), ncols=120)
-        image_tensors = []
-        for epoch in epoch_iter:
-            optimiser.zero_grad()
-            # Pass through transfer network
-            output = model(self.content_tensor)
+        update_count = 0
+        with tqdm(total=num_parameter_updates, ncols=120) as progress_bar:
+            image_tensors = []
+            while update_count < num_parameter_updates:
 
-            # Calculate loss
-            style_loss, content_loss = self.loss_network.calculate_image_loss(output, self.style_tensor, self.content_tensor)
+                for batch_num, content_tensors in enumerate(self.train_loader):
+                    if update_count >= num_parameter_updates:
+                        break
 
-            #print((style_loss, content_loss))
+                    optimiser.zero_grad()
+                    # Pass through transfer network
+                    content_tensors = content_tensors.to(self.device)
+                    output = model(content_tensors)
 
-            style_loss = style_loss.mul(style_weight)
-            content_loss = content_loss.mul(content_weight)
-            loss = content_loss.add(style_loss)
-            #print(loss)
-            # Backprop (train) transfer network
-            loss.backward()
-            optimiser.step()
-            # Update tqdm bar
-            style_loss_formatted = "%.0f" % style_loss
-            content_loss_formatted = "%.0f" % style_loss
-            epoch_iter.set_postfix(checkpoint=checkpoint, style_loss=style_loss_formatted, content_loss=content_loss_formatted)
-            # Checkpoint
-            if epoch % checkpoint_freq == 0 and epoch != epochs:
-                checkpoint_file_path = os.path.join(self.save_directory, str(checkpoint+1) + '.jpeg')
-                image_tensors.append(output)
-                save_tensor_as_image(output, checkpoint_file_path)
-                checkpoint += 1
+                    # Calculate loss
+                    style_loss, content_loss = self.loss_network.calculate_image_loss(output, self.style_tensor,
+                                                                                      content_tensors)
+                    style_loss = style_loss.mul(style_weight)
+                    content_loss = content_loss.mul(content_weight)
+                    loss = content_loss.add(style_loss)
+
+                    # Backprop (train) transfer network
+                    loss.backward()
+                    optimiser.step()
+
+                    # Update tqdm bar
+                    style_loss_formatted = "%.0f" % style_loss
+                    content_loss_formatted = "%.0f" % style_loss
+                    progress_bar.set_postfix(checkpoint=checkpoint, style_loss=style_loss_formatted,
+                                             content_loss=content_loss_formatted)
+
+                    # Checkpoint
+                    if update_count % checkpoint_freq == 0:
+                        checkpoint_file_path = os.path.join(self.save_directory, str(checkpoint+1) + '.jpeg')
+                        test_output = model(self.test_image_tensor)
+                        image_tensors.append(test_output)
+                        save_tensor_as_image(test_output, checkpoint_file_path)
+                        checkpoint += 1
+
+                    update_count += 1
+                    progress_bar.update(1)
 
         # Save image
-        final_output = model(self.content_tensor)
+        final_output = model(self.test_image_tensor)
+        image_tensors.append(self.test_image_tensor)
+        image_tensors.append(self.style_tensor)
         image_tensors.append(final_output)
 
         final_file_path = os.path.join(self.save_directory, 'final.jpeg')
@@ -184,7 +230,8 @@ class TransferNetworkTrainerSingle:
 
 if __name__ == '__main__':
     style_path = '../data/images/style/Van_Gogh_Starry_Night.jpg'
-    content_path = '../data/images/content/venice.jpeg'
+    content_dir = '/home/data/train2014/'
     save_path = '../data/images/produced/venice'
-    transfer_network = TransferNetworkTrainerSingle(style_path, content_path, save_path)
+    test_image_path = '../data/images/content/venice.jpeg'
+    transfer_network = TransferNetworkTrainerSingle(style_path, content_dir, save_path, test_image_path)
     transfer_network.train()
